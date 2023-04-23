4ed3c56fa5a075962a5fbbbd3cd9df364d18ff34
gangjoohyeong <bles@kakao.com>
2023-04-23 18:40:51 +0900

    add annotation

--- 5. flask Jinja2 template/app_condition.py
+++ 5. flask Jinja2 template/app_condition.py
@@ -9,3 +9,9 @@ def hello_html():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8080')
+    
+    
+    
+# 접속 테스트
+# http://127.0.0.1:8080/hello_condition
+# router로 age를 지정하고 싶으면 어떻게 할까?
