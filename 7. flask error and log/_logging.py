e6332f48b347be4aed05c4d2f4c49ec7fe7a91d7
gangjoohyeong <bles@kakao.com>
2023-04-25 22:21:34 +0900

    modify logging example

--- 7. flask error and log/_logging.py
+++ 7. flask error and log/_logging.py
@@ -5,14 +5,14 @@ import logging
 logging.basicConfig(filename='./test.log', level=logging.ERROR)
 
 # 로그를 남길 부분에 다음과 같이 로그 레벨에 맞추어 출력해주면 해당 내용이 파일에 들어감
-logging.debug("debug")
-logging.info("info")
-logging.warning("warning")
-logging.error("error")
-logging.critical("critical")
+logging.debug("DEBUG!!")
+logging.info("INFO!!")
+logging.warning("WARNING!!")
+logging.error("ERROR!!")
+logging.critical("CRITICAL!!")
 
 
 # test.log
 
-# ERROR:root:error
-# CRITICAL:root:critical
+# ERROR:root:ERROR!!
+# CRITICAL:root:CRITICAL!!
