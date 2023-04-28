18e91c9ec25427f59d0316f157c9e5c5f2c82708
gangjoohyeong <bles@kakao.com>
2023-04-28 09:55:07 +0900

    edit comments

--- 8. flask blueprint/app.py
+++ 8. flask blueprint/app.py
@@ -3,8 +3,10 @@ from sub_blueprint import blog_test
 
 
 app = Flask(__name__)
+# (blueprint 객체명, url_prefix: 기본 경로명)
 app.register_blueprint(blog_test.blog_ab, url_prefix='/blog')
 
+# http://127.0.0.1:8080/기본 경로명/라우팅 경로
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8080')
